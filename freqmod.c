887aa66bf92869bdbcc307c5b0b1b4ffbbd8fa03
fbergo <fbergo@gmail.com>
2018-05-01 11:16:35 -0300

    indentation

--- freqmod.c
+++ freqmod.c
@@ -7,17 +7,17 @@ extern unsigned int arch_freq_get_on_cpu(int cpu);
 
 __weak unsigned int arch_freq_get_on_cpu(int cpu)
 {
-        return 0;
+  return 0;
 }
 
 int __init freqmod_init(void) {
   unsigned int f[2];
-
+  
   printk(KERN_INFO "freqmod test init.\n");
-
-  f[0]   = arch_freq_get_on_cpu(0);
+  
+  f[0] = arch_freq_get_on_cpu(0);
   f[1] = cpufreq_get(0);
-
+  
   printk(KERN_INFO "arch freq=%u, cpufreq driver freq=%u\n",f[0],f[1]); 
   
   return 0;
